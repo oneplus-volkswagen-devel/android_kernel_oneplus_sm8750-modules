f81b29b5bebc07dd9f1fa1845dfaf5c370c23c0a
SphericalKat <amolele@gmail.com>
2026-09-22 23:32:38 +0530

    sensor: Import Ace 6 feedback updates
    Import the Ace 6 OPlus sensor delta from OnePlusOSS.
    
    Repo: android_kernel_modules_and_devicetree_oneplus_sm8750
    
    Branch: oneplus/sm8750_b_16.0.0_ace_6
    
    Path: vendor/oplus/sensor
    
    AU tag: AU_LINUX_KERNEL.PLATFORM.4.0.R1.00.00.00.061.111
    
    Sync: PLQ110_16.0.9.401(CN01)
    
    Provenance: 294f2993..b8d60e4c
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- oplus/sensor/kernel/qcom/sensor/oplus_sensor_feedback/sensor_feedback.c
+++ oplus/sensor/kernel/qcom/sensor/oplus_sensor_feedback/sensor_feedback.c
@@ -163,7 +163,14 @@ struct sensor_fb_conf g_fb_conf[] = {
 	{ACCEL_DATA_BLOCK_ID, "device_acc_data_block", SENSOR_DEVICE_TYPE},
 	{ACCEL_SUB_DATA_BLOCK_ID, "device_sub_acc_data_block", SENSOR_DEVICE_TYPE},
 	{ACCEL_DATA_FULL_RANGE_ID, "device_acc_data_full_range", SENSOR_DEVICE_TYPE},
+	{ACCEL_SW_RESET_ID, "device_acc_sw_reset", SENSOR_DEVICE_TYPE},
+	{ACCEL_NO_DATA_ID, "device_acc_no_data", SENSOR_DEVICE_TYPE},
+	{ANY_MOTION_ENABLE_FAIL_ID, "device_any_motion_enable_fail", SENSOR_DEVICE_TYPE},
+	{ACCEL_CHANGE_FSR_FAIL_ID, "device_acc_change_fsr_fail", SENSOR_DEVICE_TYPE},
 
+	{ACCEL_SUB_I2C_ERR_ID, "device_acc_sub_i2c_err", SENSOR_DEVICE_TYPE},
+	{ACCEL_SUB_NO_DATA_ID, "device_acc_sub_no_data", SENSOR_DEVICE_TYPE},
+	{ACCEL_SUB_ESD_REST_ID, "device_acc_sub_esd_reset", SENSOR_DEVICE_TYPE},
 
 	{GYRO_INIT_FAIL_ID, "device_gyro_init_fail", SENSOR_DEVICE_TYPE},
 	{GYRO_I2C_ERR_ID, "device_gyro_i2c_err", SENSOR_DEVICE_TYPE},
@@ -209,6 +216,16 @@ struct sensor_fb_conf g_fb_conf[] = {
 	{FOLD_DEVICE_FOLDE_COUNT_ID, "device_fold_count", SENSOR_DEVICE_TYPE},
 	{FOLD_DEVICE_USE_HALL_ANGLE_COUNT_ID, "device_use_hall_angle_count", SENSOR_DEVICE_TYPE},
 	{HINGE_DETECT_INTERVAL_TIME_ID, "device_hinge_detect_interval_time_id", SENSOR_DEVICE_TYPE},
+	{HINGE_DETECT_SUB_ACCEL_INIT_FAIL_ID, "device_hinge_detect_sub_accel_init_fail_id", SENSOR_DEVICE_TYPE},
+	{HINGE_DETECT_SUB_ACCEL_DATA_LOST_ID, "device_hinge_detect_sub_accel_data_lost_id", SENSOR_DEVICE_TYPE},
+	{HINGE_DETECT_FOLD_HALL_INIT_FAIL_ID, "device_hinge_detect_fold_hall_init_fail_id", SENSOR_DEVICE_TYPE},
+	{HINGE_DETECT_FOLD_HALL_DATA_LOST_ID, "device_hinge_detect_fold_hall_data_lost_id", SENSOR_DEVICE_TYPE},
+	{HINGE_DETECT_MAIN_ACCEL_INIT_FAIL_ID, "device_hinge_detect_main_accel_init_fail_id", SENSOR_DEVICE_TYPE},
+	{HINGE_DETECT_MAIN_ACCEL_DATA_LOST_ID, "device_hinge_detect_main_accel_data_lost_id", SENSOR_DEVICE_TYPE},
+	{HINGE_DETECT_MAIN_GYRO_INIT_FAIL_ID, "device_hinge_detect_main_gyro_init_fail_id", SENSOR_DEVICE_TYPE},
+	{HINGE_DETECT_MAIN_GYRO_DATA_LOST_ID, "device_hinge_detect_main_gyro_data_lost_id", SENSOR_DEVICE_TYPE},
+	{HINGE_DETECT_SUB_GYRO_INIT_FAIL_ID, "device_hinge_detect_sub_gyro_init_fail_id", SENSOR_DEVICE_TYPE},
+	{HINGE_DETECT_SUB_GYRO_DATA_LOST_ID, "device_hinge_detect_sub_gyro_data_lost_id", SENSOR_DEVICE_TYPE},
 
 	{FREE_FALL_TRIGGER_ID, "device_free_fall", SENSOR_DEVICE_TYPE},
 
